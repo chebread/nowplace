ef2a8026089a35eb631aeb1f9f0040cc0e4610d4
chebread <83535893+chebread@users.noreply.github.com>
2024-08-08 22:52:54 +0900

    feat: long press event로 현재 위치 구하기 기능 추가 bug: stop... 그게 안됨

--- src/utils/use-long-press.tsx
+++ src/utils/use-long-press.tsx
@@ -1,38 +1,61 @@
-import { useState, useEffect, useCallback } from 'react';
-import { debounce } from 'lodash';
+import { useCallback, useRef, useState } from 'react';
 
-const useLongPress = (handleLongPress: any, ms = 3000, debounceMs = 300) => {
-  const [startLongPress, setStartLongPress] = useState(false);
-  const [event, setEvent] = useState(null);
+const useLongPress = (
+  onLongPress: any,
+  onClick: any,
+  { shouldPreventDefault = true, delay = 300 } = {}
+) => {
+  const [longPressTriggered, setLongPressTriggered] = useState(false);
+  const timeout = useRef<any>();
+  const target = useRef<any>();
 
-  useEffect(() => {
-    let timerId: any;
-    if (startLongPress && event) {
-      timerId = setTimeout(() => handleLongPress(null, event), ms);
-    } else {
-      clearTimeout(timerId);
-    }
+  const start = useCallback(
+    (event: any) => {
+      if (shouldPreventDefault && event.target) {
+        event.target.addEventListener('touchend', preventDefault, {
+          passive: false,
+        });
+        target.current = event.target;
+      }
+      timeout.current = setTimeout(() => {
+        onLongPress(event);
+        setLongPressTriggered(true);
+      }, delay);
+    },
+    [onLongPress, delay, shouldPreventDefault]
+  );
 
-    return () => {
-      clearTimeout(timerId);
-    };
-  }, [handleLongPress, ms, startLongPress, event]);
-
-  const startPress = (e: any) => {
-    setEvent(e);
-    setStartLongPress(true);
-  };
-
-  const stopLongPress = () => setStartLongPress(false);
+  const clear = useCallback(
+    (event: any, shouldTriggerClick = true) => {
+      timeout.current && clearTimeout(timeout.current);
+      shouldTriggerClick && !longPressTriggered && onClick();
+      setLongPressTriggered(false);
+      if (shouldPreventDefault && target.current) {
+        target.current.removeEventListener('touchend', preventDefault);
+      }
+    },
+    [shouldPreventDefault, onClick, longPressTriggered]
+  );
 
   return {
-    onMouseDown: startPress,
-    onMouseUp: () => setStartLongPress(false),
-    onMouseLeave: () => setStartLongPress(false),
-    onTouchStart: startPress,
-    onTouchEnd: () => setStartLongPress(false),
-    stopLongPress,
+    onMouseDown: (e: any) => start(e),
+    onTouchStart: (e: any) => start(e),
+    onMouseUp: (e: any) => clear(e),
+    onMouseLeave: (e: any) => clear(e, false),
+    onTouchEnd: (e: any) => clear(e),
   };
+};
+
+const isTouchEvent = (event: any) => {
+  return 'touches' in event;
+};
+
+const preventDefault = (event: any) => {
+  if (!isTouchEvent(event)) return;
+
+  if (event.touches.length < 2 && event.preventDefault) {
+    event.preventDefault();
+  }
 };
 
 export default useLongPress;
