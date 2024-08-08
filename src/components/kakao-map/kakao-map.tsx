ea1120391821258189bfb7290a1089d899ed55a8
Cha Haneum <developer_haneum_11@icloud.com>
2024-08-08 22:52:54 +0900

    feat: long press event로 현재 위치 구하기 기능 추가 bug: stop... 그게 안됨

--- src/components/kakao-map/kakao-map.tsx
+++ src/components/kakao-map/kakao-map.tsx
@@ -1,36 +1,48 @@
 import { Map, useKakaoLoader } from 'react-kakao-maps-sdk';
 import KaKaoMapError from './kakao-map-error';
 import useLongPress from '@/utils/use-long-press';
+import { useRef, useState } from 'react';
 
 // 만약 아예 권한조차 허락받지 않은 사용자라면 버튼으로하여금 허락을 요청하기
 // - [ ] 길게 누르면 장소 추가 기능 만들기
 
 export default function KakaoMap(props: any) {
+  const mapRef = useRef<any>();
   const defaultLevel = 5; // 상수 선언 => 추후 변경값이면 useRef로 Wrapping 하기
   const defaultCenter = { lat: 37.575857, lng: 126.976805 }; // { lat: 36.3677, lng: 127.4365 };
   const [_, error]: any = useKakaoLoader({
     appkey: process.env.NEXT_PUBLIC_KAKAO_API_KEY!,
     libraries: ['clusterer', 'drawing', 'services'],
   });
+  const [latlng, setLatlng] = useState({});
 
-  const handleLongPress = (_: any, mouseEvent: any) => {
-    console.log(mouseEvent);
+  const handleLongPress = (e: any) => {
+    console.log('long press', latlng);
   };
+  const defaultOptions = {
+    shouldPreventDefault: true,
+    delay: 500,
+  };
+  const longPressEvent = useLongPress(
+    handleLongPress,
+    () => {},
+    defaultOptions
+  );
 
-  const {
-    onMouseDown,
-    onMouseUp,
-    onMouseLeave,
-    onTouchStart,
-    onTouchEnd,
-    stopLongPress,
-  } = useLongPress(handleLongPress, 1000, 100);
+  const handleMouseDown = (map: any, mouseEvent: any) => {
+    const latLng = mouseEvent.latLng;
+    setLatlng({
+      lat: latLng.getLat(),
+      lng: latLng.getLng(),
+    });
+  };
 
   if (error) return <KaKaoMapError />;
 
   return (
     <>
       <Map
+        ref={mapRef}
         style={{
           height: '100%',
           width: '100%',
@@ -40,16 +52,17 @@ export default function KakaoMap(props: any) {
         center={props?.center || defaultCenter}
         onDragEnd={map => {
           props?.onDragEnd(map);
-          stopLongPress(); // - [ ] longpress 지우기
+          // stopLongPress(); // - [ ] longpress 지우기
         }}
         onZoomStart={props?.onZoomStart || undefined}
         onDragStart={props?.onDragStart || undefined}
         /* 장소 추가 */
-        onMouseDown={onMouseDown}
-        onMouseUp={onMouseUp}
-        onMouseLeave={onMouseLeave}
-        onTouchStart={onTouchStart}
-        onTouchEnd={onTouchEnd}
+        {...longPressEvent}
+        onCreate={map => {
+          kakao.maps.event.addListener(map, 'mousedown', (mouseEvent: any) =>
+            handleMouseDown(map, mouseEvent)
+          );
+        }}
       >
         {props.children}
       </Map>
