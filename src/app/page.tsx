6e849cc62040aba439fa86633e3d1fbabdca91be
Cha Haneum <developer_haneum_11@icloud.com>
2024-08-13 00:39:12 +0900

    fix: onCurPosTracking 클릭시 이 장소 검색 기능 안뜨는 문제 해결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -195,6 +195,7 @@ export default function Home() {
   // 손상된 데이터(빈 값도 포함)가 발생시 null 반환함
   const fetchDataFromUrl = (key: string) => {
     // - [ ] 지금 오류는 saveData에서만 일어남. 일단 임시로 fetchDataFromUrl을 saveData에서 handleBounds할때 쓰는게 아니고, 일단 saveData에서 생성한 데이터를 가지고 handBoudns에서 사용하도록 함
+    // const searchParams = new URLSearchParams(window.location.search);
     // console.log('xxx', searchParams); // - [ ] 이게 useState같은 거라서 바로 반영이 안됨. 렌더링 한다음에 반영해야함.
     let returnValue = null;
     console.log(10000);
@@ -212,7 +213,6 @@ export default function Home() {
       returnValue = null;
     }
     console.log(50000);
-    console.log(returnValue);
     return returnValue;
   };
   // URL에 Base64 데이터 저장하기
@@ -287,7 +287,7 @@ export default function Home() {
   const handleBoundsChanged = () => {
     console.log(2);
     const fetchedData: any = fetchDataFromUrl('data');
-    console.log(fetchedData);
+    //console.log(fetchedData);
 
     if (isNil(fetchedData)) {
       console.log('오류 발생이거나, 데이터에 아무런 값이 없습니다');
@@ -337,6 +337,7 @@ export default function Home() {
   const onCurPosTracking = () => {
     setCenterPos(curPos);
     setIsTracking(true);
+    setMapMovedToggle(true); // 이동됨을 알림
   };
   const handleGeoError = (error: any) => {
     switch (error.code) {
