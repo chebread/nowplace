8686e913fcf0bc49a9690eeb2acf01866a21b16c
Cha Haneum <developer_haneum_11@icloud.com>
2024-08-12 21:00:16 +0900

    refactor: place를 data로 재명명함

--- src/app/home.css.ts
+++ src/app/home.css.ts
@@ -5,30 +5,107 @@ import {
 import TextareaAutosize from 'react-textarea-autosize';
 import styled, { keyframes } from 'styled-components';
 
-export const StyledMain = styled.main``;
+export const StyledMain = styled.main`
+  position: relative;
+  height: 100dvh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+`;
 
 export const StyledMap = styled.div`
-  position: relative;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   z-index: 0;
 `;
 
 export const StyledHeaderLayout = styled.div``;
 
 export const StyledHeader = styled.header`
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 0;
-  z-index: 1;
-  height: auto;
-  width: 100%;
-  backdrop-filter: blur(1.5rem);
+  margin: 0.5rem 1rem 0rem 1rem;
+  backdrop-filter: blur(1rem);
+  -webkit-backdrop-filter: blur(1rem);
   background-color: rgba(255, 255, 255, 0.85);
-  svg {
-    fill: #000000;
+  height: 4rem;
+  max-width: 40rem;
+  width: 100%;
+  z-index: 0;
+  padding: 1rem;
+  box-sizing: border-box;
+  z-index: 1;
+  border-radius: 1rem;
+  box-shadow: 0 12px 24px 0 rgba(0, 0, 0, 0.08);
+`;
+
+// PlaceFetcher로 하면x, 데이터를 불러오는 거라고 명명해야함
+export const DataFetcherBtnWrapper = styled.div`
+  position: absolute;
+  top: 0;
+
+  margin: 1rem;
+  @media screen and (max-width: 641px) {
+    margin: 0.5rem;
   }
+
+  will-change: transform;
+  z-index: 1;
+
+  -webkit-touch-callout: none;
+  -khtml-user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  -webkit-tap-highlight-color: transparent;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+
+  animation-duration: 0.2s;
+  animation-timing-function: ease-out;
+  animation-name: smooth;
+
+  @keyframes smooth {
+    from {
+      opacity: 0;
+      transform: scale(0);
+    }
+
+    to {
+      opacity: 1;
+      transform: scale(1);
+    }
+  }
+`;
+
+export const DataFetcherBtn = styled.button`
+  all: unset;
+  cursor: pointer;
+  will-change: transform;
+
+  transition-property: transform;
+  transition-duration: 0.2s;
+  transition-timing-function: ease-out;
+
+  &:active {
+    transform: scale(0.86);
+  }
+  padding: 0 1rem;
+
+  backdrop-filter: blur(1rem);
+  -webkit-backdrop-filter: blur(1rem);
+  background-color: rgba(255, 255, 255, 0.85);
+
+  height: 3.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg {
+    height: 2rem;
+    width: 2rem;
+  }
+  border-radius: 9999px;
+  box-shadow: 0 10.5px 21px rgba(0, 0, 0, 0.08);
+  font-size: 1rem;
+  font-weight: 500;
 `;
 
 export const StyledFooterLayout = styled.div`
@@ -37,8 +114,8 @@ export const StyledFooterLayout = styled.div`
   right: 0;
   bottom: 0;
   z-index: 1;
-  height: 4rem; // 64px
-  width: 100vw;
+  height: auto;
+  width: 100%;
   backdrop-filter: blur(1rem);
   -webkit-backdrop-filter: blur(1rem);
   background-color: rgba(255, 255, 255, 0.85);
@@ -47,8 +124,9 @@ export const StyledFooterLayout = styled.div`
 `;
 
 export const StyledFooter = styled.footer`
-  height: 100%;
-  width: 40rem;
+  height: 4rem;
+  max-width: 40rem;
+  width: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -138,23 +216,23 @@ export const CurPosMarker = styled.div`
   background-color: var(--blue-500); // apple map: rgb(0, 122, 254)
 `;
 
-export const AddPlaceDrawerModal = styled(DrawerModal)`
+export const AddDataDrawerModal = styled(DrawerModal)`
   height: 100%;
   width: 100%;
 `;
 
-export const AddPlaceDrawerContents = styled(DrawerContents)`
+export const AddDataDrawerContents = styled(DrawerContents)`
   height: 100%;
   width: 100%;
 `;
 
-export const AddPlaceTextareaWrapper = styled.div`
+export const AddDataTextareaWrapper = styled.div`
   display: flex;
   width: 100%;
   background-color: #ffffff; // textarea 회색 잔상 제거
 `;
 
-export const AddPlaceTextarea = styled(TextareaAutosize)`
+export const AddDataTextarea = styled(TextareaAutosize)`
   white-space: normal;
   overflow-wrap: break-word;
   word-wrap: break-word;
@@ -163,11 +241,12 @@ export const AddPlaceTextarea = styled(TextareaAutosize)`
   outline: 0;
   overflow: auto;
   border-radius: 0;
-  -webkit-tap-highlight-color: transparent; // 비표준
+  -webkit-tap-highlight-color: transparent;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
   width: 100%;
 `;
 
-export const AddPlaceFooter = styled.footer`
+export const AddDataFooter = styled.footer`
   position: fixed;
   bottom: 0;
   left: 0;
@@ -176,7 +255,7 @@ export const AddPlaceFooter = styled.footer`
   margin: 0 auto;
 `;
 
-export const AddPlaceFooterGradient = styled.div`
+export const AddDataFooterGradient = styled.div`
   height: 2rem;
   background: linear-gradient(
     to top,
@@ -185,12 +264,12 @@ export const AddPlaceFooterGradient = styled.div`
   );
 `;
 
-export const AddPlaceFooterWrapper = styled.div`
+export const AddDataFooterWrapper = styled.div`
   padding: 1rem;
   background-color: #ffffff;
 `;
 
-export const AddPlaceFooterBtn = styled.button`
+export const AddDataFooterBtn = styled.button`
   height: 3rem;
   width: 100%;
   font-weight: 600;
