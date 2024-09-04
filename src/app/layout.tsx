279eed6959bcc231e05979b6abfc43deea5e216b
chebread <83535893+chebread@users.noreply.github.com>
2024-09-04 20:38:17 +0900

    feat: title change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
   metadataBase: new URL('https://nowplace.kr/'),
   title: {
     template: '%s | NowPlace',
-    default: '장소 저장소, NowPlace',
+    default: '장소 저장소 | NowPlace',
   },
   description: 'NowPlace는 장소를 저장하고 공유하는 위치 기반 서비스입니다.',
 };
